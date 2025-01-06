aa67556c484d0b324c6ea5c9d31d45e81d1f7e7c
Arsen <arsendacenko89@gmail.com>
2025-01-06 14:41:50 +0200

    Last update

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import MainAppRender from './App';
-
-test('renders learn react link', () => {
-  render(<MainAppRender />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
